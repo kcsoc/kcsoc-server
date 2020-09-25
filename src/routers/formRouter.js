ff3e02cc5ebd709ce9a5f403bb83772e12eb86b3
Josh Guha <joshguha@gmail.com>
2020-09-25 10:47:59 +0100

    Form Router correctly pulls google keys from the .env file

--- src/routers/formRouter.js
+++ src/routers/formRouter.js
@@ -2,76 +2,90 @@ const router = require("express").Router();
 const moment = require("moment");
 const { google } = require("googleapis");
 require("dotenv").config();
-const keys = JSON.parse(process.env.GOOGLE_KEYS.replace(/•/g, "\n"));
 
-const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
-  "https://www.googleapis.com/auth/spreadsheets",
-]);
+const client = new google.auth.JWT(
+	process.env.GOOGLE_KEYS_CLIENT_EMAIL,
+	null,
+	process.env.GOOGLE_KEYS_PRIVATE_KEY,
+	["https://www.googleapis.com/auth/spreadsheets"]
+);
 
 client.authorize(function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+	if (err) {
+		console.log(err);
+		return;
+	}
 
-  console.log("Connected to Google Sheets!");
+	console.log("Connected to Google Sheets!");
 });
 
 const gsapi = google.sheets({ version: "v4", auth: client });
 
 router.post("/get-involved", async (req, res) => {
-  try {
-    const { firstName, lastName, university, email, phoneNumber } = req.body;
+	try {
+		const {
+			firstName,
+			lastName,
+			university,
+			email,
+			phoneNumber,
+		} = req.body;
 
-    const newRow = [
-      [
-        firstName,
-        lastName,
-        university,
-        email,
-        phoneNumber,
-        moment().format("DD/MM/YYYY"),
-      ],
-    ];
+		const newRow = [
+			[
+				firstName,
+				lastName,
+				university,
+				email,
+				phoneNumber,
+				moment().format("DD/MM/YYYY"),
+			],
+		];
 
-    const updateOptions = {
-      spreadsheetId: "1ywgsA221E8TbulZ0LXAzZMWn0MXP9yCd4JeAsy3vDuc",
-      range: "Get Involved!A1",
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: newRow,
-      },
-    };
+		const updateOptions = {
+			spreadsheetId: "1ywgsA221E8TbulZ0LXAzZMWn0MXP9yCd4JeAsy3vDuc",
+			range: "Get Involved!A1",
+			valueInputOption: "USER_ENTERED",
+			resource: {
+				values: newRow,
+			},
+		};
 
-    await gsapi.spreadsheets.values.append(updateOptions);
-    return res.status(201).send({ msg: "Form submission accepted" });
-  } catch (e) {
-    return res.status(500).send({ error: e.message });
-  }
-});	
+		await gsapi.spreadsheets.values.append(updateOptions);
+		return res.status(201).send({ msg: "Form submission accepted" });
+	} catch (e) {
+		return res.status(500).send({ error: e.message });
+	}
+});
 
 router.post("/set-up-a-kcsoc", async (req, res) => {
-  try {
-    const { firstName, lastName, email, phoneNumber } = req.body;
+	try {
+		const { firstName, lastName, email, phoneNumber } = req.body;
 
-    const newRow = [
-      [firstName, lastName, email, phoneNumber, moment().format("DD/MM/YYYY")],
-    ];
+		const newRow = [
+			[
+				firstName,
+				lastName,
+				email,
+				phoneNumber,
+				moment().format("DD/MM/YYYY"),
+			],
+		];
 
-    const updateOptions = {
-      spreadsheetId: "1ywgsA221E8TbulZ0LXAzZMWn0MXP9yCd4JeAsy3vDuc",
-      range: "Set Up A KCSOC!A1",
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: newRow,
-      },
-    };
+		const updateOptions = {
+			spreadsheetId: "1ywgsA221E8TbulZ0LXAzZMWn0MXP9yCd4JeAsy3vDuc",
+			range: "Set Up A KCSOC!A1",
+			valueInputOption: "USER_ENTERED",
+			resource: {
+				values: newRow,
+			},
+		};
 
-    await gsapi.spreadsheets.values.append(updateOptions);
-    return res.status(201).send({ msg: "Form submission accepted" });
-  } catch (e) {
-    return res.status(500).send({ error: e.message });
-  }
+		await gsapi.spreadsheets.values.append(updateOptions);
+		return res.status(201).send({ msg: "Form submission accepted" });
+	} catch (e) {
+		return res.status(500).send({ error: e.message });
+	}
 });
 
 module.exports = router;
